a9ec4596e74a9eb4af0a100584990cac42a3b273
Shinya Hijiri <shinya.hijiri@gmail.com>
2018-06-10 15:59:59 +0900

    Disabledタグを使う

---
+++ src/test/java/practice/DisablingTests/DisabledTestClass.java
@@ -0,0 +1,18 @@
+package practice.DisablingTests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Disabled
+class DisabledTestClass {
+
+    // IDE等で直接テストクラスを実行した場合はスキップされないので注意
+
+    @Test
+    void testWillBeSkipped() {
+        //　テストクラスが無効になっているので、テストは全てスキップされる
+        assertEquals(1, 2);
+    }
+}
---
+++ src/test/java/practice/DisablingTests/DisabledTests.java
@@ -0,0 +1,17 @@
+package practice.DisablingTests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class DisabledTests {
+
+    @Disabled
+    @Test
+    void testWillBeSkipped() {
+
+    }
+
+    @Test
+    void testWillBeExecuted() {
+    }
+}
